b5362b744587c6238e495e31354806e162c5080c
Yuriy Poltaev <yuriy.poltaev@gmail.com>
2020-06-21 12:33:48 +0400

    First program added

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        String hello="Hello ";
+        String world="world";
+        System.out.println(hello+world+"!");
+    }
+}
